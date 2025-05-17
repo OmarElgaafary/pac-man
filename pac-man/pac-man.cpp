fdbb9d052cb6b235d10e5674d79e6eb655bc7150
Omar Adly <omarelgaafaryy@gmail.com>
2025-05-17 22:41:23 +0300

    Added wall collisions.

--- pac-man/pac-man.cpp
+++ pac-man/pac-man.cpp
@@ -1,59 +1,75 @@
 #include <raylib.h>
 
 int Xres = 560, Yres = 440;
+Rectangle Blocks[650];
 
 class PacMan {
 public:
 
 	void checkLogic() {
 
+		pac_direction.x = (float)IsKeyDown(KEY_RIGHT) - (float)IsKeyDown(KEY_LEFT);
+		pac_direction.y = (float)IsKeyDown(KEY_DOWN) - (float)IsKeyDown(KEY_UP);
 
-		if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
+		if (IsKeyDown(KEY_RIGHT))
 		{
-			position.x += 5;
+			position.x += 2.5;
 			if (framerec.width < 0)
 				framerec.width = -framerec.width;
-			isVer = false;
+			isVertical = false;
 		}
-		else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
+		else if (IsKeyDown(KEY_LEFT))
 		{
-			position.x -= 5;
+			position.x -= 2.5;
 			if (framerec.width > 0)
 				framerec.width = -framerec.width;
-			isVer = false;
+			isVertical = false;
 		}
-		else if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
+		else if (IsKeyDown(KEY_UP))
 		{
-			isVer = true;
-			position.y -= 5;
+			isVertical = true;
+			position.y -= 2.5;
 			if (framerec.height < 0)
 				framerec.height = -framerec.height;
 
 
 		}
-		else if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
+		else if (IsKeyDown(KEY_DOWN))
 		{
-			isVer = true;
-			position.y += 5;
+			isVertical = true;
+			position.y += 2.5;
 			if (framerec.height > 0)
 				framerec.height = -framerec.height;
 		}
 
 		++frameIndex;
 		framerec.x = (float)frameWidth * frameIndex;
-
 	}
 
-	void checkCollision()
+	void checkCollisions(Rectangle rec[])
 	{
-
+		Rectangle hitbox = getHitbox();
+		for (int i = 0; i < 620; ++i)
+		{
+			if (CheckCollisionRecs(rec[i], hitbox))
+			{
+				if (pac_direction.x > 0)
+					position.x = rec[i].x - hitbox.width;
+				else if (pac_direction.x < 0)
+					position.x = rec[i].x + hitbox.width;
+				else if (pac_direction.y > 0)
+					position.y = rec[i].y - hitbox.height;
+				else if (pac_direction.y < 0)
+					position.y = rec[i].y + hitbox.height;
+			}
+		}
 	}
 
 	void Draw()
 	{
-		if (isVer)
+		if (isVertical)
 			DrawTextureRec(pac_up, framerec, position, WHITE);
-		else if (!isVer)
+		else if (!isVertical)
 			DrawTextureRec(pac_right, framerec, position, WHITE);
 
 	}
@@ -64,21 +80,16 @@ public:
 		return hitbox;
 	}
 
-	Vector2 getPacPosition()
-	{
-		return position;
-	}
-
-
 
 private:
-	bool isVer = false;
-	Texture2D pac_right = LoadTexture("C:/Users/Omar/Desktop/Pac/pac-man-h.png");
-	Texture2D pac_up = LoadTexture("C:/Users/Omar/Desktop/Pac/pac-man-v.png");
+	bool isVertical = false;
+	Texture2D pac_right = LoadTexture("C:/Users/Omar/Desktop/Pac/pac-man/pac-man-h.png");
+	Texture2D pac_up = LoadTexture("C:/Users/Omar/Desktop/Pac/pac-man/pac-man-v.png");
 	int frameWidth = pac_right.width / 9;
 	int frameIndex = 0;
 	Rectangle framerec = { 0.0f, 0.0f, (float)pac_right.width / 9 , (float)pac_right.height };
-	Vector2 position = { 100, 100 };
+	Vector2 position = { 20, 20 };
+	Vector2 pac_direction = {1, 1};
 };
 
 
@@ -97,40 +108,27 @@ public:
 					int x_position = j * blockSize;
 					int y_position = i * blockSize;
 
-					Rectangle block = { x_position, y_position, blockSize, blockSize };
+					Rectangle block = { (float)x_position, (float)y_position, (float)blockSize, (float)blockSize };
+					if (blockCount < 620){
+						Blocks[blockCount] = block;
+						blockCount++;
+					}
 					DrawRectangleRec(block, mapcolor);
 				}
 			}
 		}
 	}
 
-	Rectangle getMapFrame()
-	{
-		return pacMap;
-	}
 
 private:
+	int blockCount = 0;
 	int blockSize = 20;
 	Color mapcolor = { 0,0,139,200 };
 	Rectangle pacMap = { 50, 50, 550, 650 };
-	Rectangle Blocks[50] = {};
 
 };
 
-
-int main()
-
-{
-	SetTargetFPS(60);
-
-
-	InitWindow(Xres, Yres, "Pac-Man indevelopment by Omar Adly");
-
-	Map pacMap;
-	PacMan pac;
-
-
-	int Grid[31][28] = {
+int Grid[22][28] = {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	{1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
@@ -153,32 +151,40 @@ int main()
 	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-	};
+};
 
+
+int main()
+
+{
+	SetTargetFPS(60);
+
+
+	InitWindow(Xres, Yres, "Pac-Man indevelopment by Omar Adly");
+
+	Map pacMap;
+	PacMan pac;
 
 
 	while (!WindowShouldClose())
 	{
 
 
-
 		BeginDrawing();
 
 
-		pacMap.createGrid(Grid, 31, 28);
+		pac.checkCollisions(Blocks);
 		pac.checkLogic();
-
 
 		ClearBackground(BLACK);
 
+		pacMap.createGrid(Grid, 22, 28);
 		pac.Draw();
-
-
 
 		EndDrawing();
 	}
 
 	CloseWindow();
-
+	
 	return 0;
 }
